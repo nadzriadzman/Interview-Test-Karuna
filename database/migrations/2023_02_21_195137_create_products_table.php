69ebd0ae467038f280efeeee7e4b44eb8965fce9
NadzriAdzman <nadzri@pacomsolution.com>
2023-02-22 04:05:59 +0800

    add migration

---
+++ database/migrations/2023_02_21_195137_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table)
+        {
+            $table->increments('id'); // autoincrement id field
+            $table->string('name', 255)->nullable();
+            $table->float('price', 10, 2)->nullable();
+            $table->string('details', 255)->nullable();
+            $table->integer('publish')->nullable();  
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
